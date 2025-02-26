cbce10e51e3f75ad037f750f0d17deedfdd3d4bb
Vani85 <vani.natraj@gmail.com>
2025-02-26 17:34:19 +0000

    month fix

--- SupportBank/Utils/Utility.cs
+++ SupportBank/Utils/Utility.cs
@@ -7,7 +7,7 @@ namespace Utils{
             return Convert.ToInt32(float.Parse(amount) * 100);   
         }
         public static DateTime DateConverterStringToDate(string date) {           
-            return DateTime.ParseExact(date, "dd/mm/yyyy", null).Date;  
+            return DateTime.ParseExact(date, "dd/MM/yyyy", null).Date;  
         }
     }
 }
