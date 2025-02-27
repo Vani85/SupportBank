65c9e743f46c220b34aa9e7a09c8ca37dca095a9
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-02-27 13:58:42 +0000

    Add Logging

--- SupportBank/FileReader/ReadFile.cs
+++ SupportBank/FileReader/ReadFile.cs
@@ -1,31 +1,50 @@
 using System.Data;
+using NLog;
 using Transaction;
 using Utils;
 namespace FileReader {
     class ReadFile {
-        public static string FileName = "Transactions2014.csv";
-        public static List<TransactionDetails> ReadTransactionDetailsFromCsvFile() {
+        private static readonly ILogger Logger = LogManager.GetLogger("File Logger");
+     
+        public static string[] GetListOfAllFiles (){
+            string FolderPath = Directory.GetCurrentDirectory()+"\\FileReader\\Input_Files";
+            string [] FileList = Directory.GetFiles(FolderPath);
+            return FileList;
+        }
+        public static List<TransactionDetails> ReadTransactionDetailsFromCsvFile(string FileName) {
             List<TransactionDetails> transactions = new List<TransactionDetails>();
-            using(var reader = new StreamReader(@"./FileReader/"+FileName))
-            {
-                int index = 0;
-                while (!reader.EndOfStream)
+            Logger.Info("Reading File: "+ FileName+"........");
+            try {
+                using(var reader = new StreamReader(@FileName))
                 {
-                    var line = reader.ReadLine();
-                    var values = line?.Split(',');
-                    if(index != 0) {
-                        TransactionDetails transaction = new TransactionDetails(
-                            Utility.DateConverterStringToDate(values[0].Trim()), 
-                            values[1].Trim(),
-                            values[2].Trim(),
-                            values[3].Trim(), 
-                            Utility.MoneyConverterStringToInt(values[4].Trim()));
-                        transactions.Add(transaction);
+                    int index = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        try{
+                            var line = reader.ReadLine();
+                            var values = line?.Split(',');
+                            if(index != 0) {
+                                TransactionDetails transaction = new TransactionDetails(
+                                    Utility.DateConverterStringToDate(values[0].Trim()), 
+                                    values[1].Trim(),
+                                    values[2].Trim(),
+                                    values[3].Trim(), 
+                                    Utility.MoneyConverterStringToInt(values[4].Trim()));
+                                transactions.Add(transaction);
+                            }
+                            index++;  
+                        }catch(System.FormatException exception){
+                            Logger.Error("Error occurred at line " + index + " of file : " + FileName + " : " + exception.Message);
+                        }     
                     }
-                    index++;   
-                }
-            }      
-            return transactions;      
+
+                } 
+                Logger.Info(FileName+" read successfully and transactions captured");   
+            }catch(FileNotFoundException exception){
+                Logger.Error("Error occured while reading the file :" + FileName +" : "+exception.Message);                
+            }               
+            
+            return transactions;    
         }
     }        
 }
--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -2,35 +2,46 @@
 using Transaction;
 using Account;
 using Reports;
+using NLog;
+using Logging;
+using NLog.Targets;
+using NLog.Config;
+using NLog.LayoutRenderers;
 
 
 namespace SupportBank {
     class SupportBank {
-        
+        private static readonly ILogger Logger = LogManager.GetLogger("File Logger");
         public static List<TransactionDetails>? transactions = null;
         public static void Main() {
-            transactions = ReadFile.ReadTransactionDetailsFromCsvFile();
+            LogConfig.ConfigureLog();
+            Logger.Info("Start of the Support Bank Program");
+            string[] FileList = ReadFile.GetListOfAllFiles();
+            for (int i = 0; i < FileList.Count(); i++){
+                string FileName = FileList[i];
+                transactions = ReadFile.ReadTransactionDetailsFromCsvFile(FileName);           
+           
+                foreach (var transaction in transactions){
+                    string from = transaction.TransactionFromPerson;
+                    string to = transaction.TransactionToPerson;
+                    DateTime date = transaction.TransactionDate;
+                    string activity = transaction.ActivityName;
+                    int amount = transaction.TransactionAmount;
 
-            foreach (var transaction in transactions){
-                string from = transaction.TransactionFromPerson;
-                string to = transaction.TransactionToPerson;
-                DateTime date = transaction.TransactionDate;
-                string activity = transaction.ActivityName;
-                int amount = transaction.TransactionAmount;
+                    if(!AccountHelper.IsAccountAvailableForUser(from)){
+                        AccountHelper.CreateNewAccount(from);
+                    }
 
-                if(!AccountHelper.IsAccountAvailableForUser(from)){
-                    AccountHelper.CreateNewAccount(from);
+                    if(!AccountHelper.IsAccountAvailableForUser(to)){
+                        AccountHelper.CreateNewAccount(to);
+                    }
+
+                    AccountHelper.UpdateTransactionDetailsInAccount(from,new TransactionDetails(date,from,to,activity,amount),0,amount);           
+                    AccountHelper.UpdateTransactionDetailsInAccount(to,new TransactionDetails(date,from,to,activity,amount),amount,0);
+                    
                 }
-
-                if(!AccountHelper.IsAccountAvailableForUser(to)){
-                    AccountHelper.CreateNewAccount(to);
-                }
-
-                AccountHelper.UpdateTransactionDetailsInAccount(from,new TransactionDetails(date,from,to,activity,amount),0,amount);           
-                AccountHelper.UpdateTransactionDetailsInAccount(to,new TransactionDetails(date,from,to,activity,amount),amount,0);
-                
-            }
-            ReportGenerator.GenerateReport();
+                ReportGenerator.GenerateReport();
+            }    
         }
     }
 }
